ca6fcc02c8da52e90975e7dfa886bc229fdfae4f
kaanyagci <kaan.yagci@makepad.fr>
2024-08-11 02:49:18 +0200

    chore(cmd/root): updated add-app and chart targets.
    This adds a new command with only 1 argument the chart or the application name instead of passing the proect-name.
    
    If only one argument provided to add-* targets, it will use the project from the current working directory
    
    Signed-off-by: kaanyagci <kaan.yagci@makepad.fr>

--- cmd/root.go
+++ cmd/root.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/Makepad-fr/kickstart/internal/application"
 	"github.com/Makepad-fr/kickstart/internal/chart"
@@ -11,10 +12,7 @@ import (
 
 func Execute() {
 	if len(os.Args) < 2 {
-		fmt.Println("Usage:")
-		fmt.Println("  kickstart init-project <project-name>       - Create initial project structure")
-		fmt.Println("  kickstart addchart <project-name> <chart-name> - Add a new chart")
-		fmt.Println("  kickstart addapp <project-name> <app-name>     - Add a new application")
+		printUsage()
 		return
 	}
 
@@ -32,31 +30,47 @@ func Execute() {
 		}
 
 	case "add-chart":
-		if len(os.Args) != 4 {
-			fmt.Println("Usage: kickstart add-chart <project-name> <chart-name>")
+		var projectName, chartName string
+		if len(os.Args) == 4 {
+			projectName = os.Args[2]
+			chartName = os.Args[3]
+		} else if len(os.Args) == 3 {
+			projectName = "."
+			chartName = os.Args[2]
+		} else {
+			fmt.Println("Usage: kickstart add-chart <project-name> <chart-name> or kickstart add-chart <chart-name>")
 			return
 		}
-		projectName := os.Args[2]
-		chartName := os.Args[3]
-		if err := chart.AddChart(projectName, chartName); err != nil {
+		if err := chart.AddChart(filepath.Clean(projectName), chartName); err != nil {
 			fmt.Printf("Error adding chart: %v\n", err)
 		}
 
 	case "add-app":
-		if len(os.Args) != 4 {
-			fmt.Println("Usage: kickstart add-app <project-name> <app-name>")
+		var projectName, appName string
+		if len(os.Args) == 4 {
+			projectName = os.Args[2]
+			appName = os.Args[3]
+		} else if len(os.Args) == 3 {
+			projectName = "."
+			appName = os.Args[2]
+		} else {
+			fmt.Println("Usage: kickstart add-app <project-name> <app-name> or kickstart add-app <app-name>")
 			return
 		}
-		projectName := os.Args[2]
-		appName := os.Args[3]
-		if err := application.AddApplication(projectName, appName); err != nil {
+		if err := application.AddApplication(filepath.Clean(projectName), appName); err != nil {
 			fmt.Printf("Error adding application: %v\n", err)
 		}
 
 	default:
-		fmt.Println("Invalid command. Use one of the following:")
-		fmt.Println("  kickstart init-project <project-name>       - Create initial project structure")
-		fmt.Println("  kickstart add-chart <project-name> <chart-name> - Add a new chart")
-		fmt.Println("  kickstart add-app <project-name> <app-name>     - Add a new application")
+		printUsage()
 	}
+}
+
+func printUsage() {
+	fmt.Println("Usage:")
+	fmt.Println("  kickstart init-project <project-name>               - Create initial project structure")
+	fmt.Println("  kickstart add-chart <project-name> <chart-name>     - Add a new chart (project-name optional)")
+	fmt.Println("  kickstart add-chart <chart-name>                    - Add a new chart to the current directory")
+	fmt.Println("  kickstart add-app <project-name> <app-name>         - Add a new application (project-name optional)")
+	fmt.Println("  kickstart add-app <app-name>                        - Add a new application to the current directory")
 }
