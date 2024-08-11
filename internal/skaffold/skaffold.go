4826c37c7016a29ffe9150d53139061ebf64d635
kaanyagci <kaan.yagci@makepad.fr>
2024-08-11 02:54:27 +0200

    fix(internal/skaffold): fix the generated skaffold.yaml file format
    This gets the latest version of skaffold.yaml from Skaffold's  source code
    
    This also changes the used yaml library to avoid the unwanted behavior with the keys casing
    
    Signed-off-by: kaanyagci <kaan.yagci@makepad.fr>

--- internal/skaffold/skaffold.go
+++ internal/skaffold/skaffold.go
@@ -1,14 +1,20 @@
 package skaffold
 
 import (
+	"encoding/base64"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
+	"regexp"
 
-	"sigs.k8s.io/yaml"
+	"gopkg.in/yaml.v3"
 )
 
 type SkaffoldConfig struct {
-	APIVersion string `yaml:"apiVersion"`
+	APIVersion string `yaml:"apiVersion"` // Change to lowercase
 	Kind       string `yaml:"kind"`
 	Deploy     Deploy `yaml:"deploy"`
 }
@@ -26,9 +32,61 @@ type Release struct {
 	ChartPath string `yaml:"chartPath"`
 }
 
+// GitHubFileContent represents the content of a file from the GitHub API response
+type GitHubFileContent struct {
+	Content  string `json:"content"`
+	Encoding string `json:"encoding"`
+}
+
+func getLatestSkaffoldYamlVersion() (string, error) {
+	// GitHub API URL for the specific file
+	url := "https://api.github.com/repos/GoogleContainerTools/skaffold/contents/docs-v2/content/en/docs/references/cli/_index.md"
+
+	// Make the HTTP GET request
+	resp, err := http.Get(url)
+	if err != nil {
+		return "", fmt.Errorf("error making the HTTP request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Check if the request was successful
+	if resp.StatusCode != http.StatusOK {
+		return "", fmt.Errorf("error: unable to fetch the file. Status code: %d", resp.StatusCode)
+	}
+
+	// Decode the JSON response
+	var fileContent GitHubFileContent
+	if err := json.NewDecoder(resp.Body).Decode(&fileContent); err != nil {
+		return "", fmt.Errorf("error decoding the JSON response: %w", err)
+	}
+
+	// Decode the base64-encoded content
+	contentBytes, err := base64.StdEncoding.DecodeString(fileContent.Content)
+	if err != nil {
+		return "", fmt.Errorf("error decoding base64 content: %w", err)
+	}
+	content := string(contentBytes)
+
+	// Define the regular expression to extract the version
+	re := regexp.MustCompile(`--version='skaffold/(v[0-9a-zA-Z]+)'`)
+
+	// Find the version in the file content
+	matches := re.FindStringSubmatch(content)
+	if len(matches) > 1 {
+		return matches[1], nil
+	} else {
+		return "", fmt.Errorf("version not found in the file")
+	}
+}
+
 func InitializeSkaffold(skaffoldPath string) error {
+	skaffoldYamlVersion, err := getLatestSkaffoldYamlVersion()
+	if err != nil {
+		log.Printf("Error while getting latest version from GitHub: %v", err)
+		return err
+	}
 	initialContent := SkaffoldConfig{
-		APIVersion: "skaffold/v2beta26",
+		APIVersion: fmt.Sprintf("skaffold/%s", skaffoldYamlVersion),
 		Kind:       "Config",
 		Deploy: Deploy{
 			Helm: Helm{
